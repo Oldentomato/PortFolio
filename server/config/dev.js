2e0ef4d0abb93e7e7f5150d3d6ddabcebebb921c
Oldentomato <ender1412@naver.com>
2021-10-11 15:38:30 +0900

    첫 커밋

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Oldentomato:jowoosung123@examplecluster.g7o5t.mongodb.net/SecondDatabase?retryWrites=true&w=majority'
+}
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose')
+const config = require('./config/key.js')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use('api/board', require('./routes/boards'))
+
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=> console.log('MongoDB Connected...'))
+  .catch(err=> console.log(err))
+
+app.listen(port,()=>{ console.log(`PorfolioApp listening on port ${port}!`)})
