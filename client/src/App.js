58fdcbd8c19a7457f0b5fae2bb497be8eb144509
조우성 <jowoosung@jouseong-ui-MacBookAir.local>
2021-11-15 22:34:18 +0900

    obj에서 gltf로 변환후 적용완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import {Switch, Route} from "react-router-dom"
 import LandingPage from "../src/components/views/LandingPage/LandingPage"
+import BoardPage from "../src/components/views/BoardPage/BoardPage"
 import "./css/App.css"
 
 export default function App() {
   return (
     <div>
-      <LandingPage/>
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/Boards" component={BoardPage}/>
+      </Switch>
     </div>
 
   );
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,11 @@
 import React,{Suspense} from "react"
 import {Canvas} from "react-three-fiber"
-import {Html, useGLTFLoader, OrbitControls} from "drei"
+import {Html, useGLTFLoader, OrbitControls, Plane} from "drei"
 import {Section} from "../../section"
 
 
 function Model(){
-    const gltf = useGLTFLoader("/Object/scene.gltf",true);
+    const gltf = useGLTFLoader("/Object/test.gltf",true);
     return <primitive object={gltf.scene} dispose={null}/>;
 }
 
@@ -13,20 +13,41 @@ const HTMLContent = () => {
     return (
         <Section factor={1.5} offset={1}>
             <group position={[0,250,0]}>
-            <mesh position={[0,-35,0]} scale={[50,50,50]}>
+            <mesh castShadow receiveShadow position={[0,80,0]} scale={[100,100,100]}>
                 <Model/>
             </mesh>
-        <Html fullscrenn>
-            <div className='container'>
-                <h1 className='title' style={{color:"#fff"}}>Hello</h1>
-            </div>
-        </Html>
+
+  
         </group>
         </Section>
 
 
     )
 }
+const Lights = () => {
+    return (
+      <>
+        {/* Ambient Light illuminates lights for all objects */}
+        <ambientLight intensity={0.3} />
+        {/* Diretion light */}
+        {/* <directionalLight position={[10, 10, 5]} intensity={1} /> */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* Spotlight Large overhead light */}
+        {/* <spotLight intensity={1} position={[10, 0, 0]} castShadow /> */}
+      </>
+    );
+  };
 
 
 
@@ -34,10 +55,18 @@ const HTMLContent = () => {
 function LandingPage(){
     return(
         <div id="world">
-            <Canvas colorManagement camera={{position:[0,0,120], fov:70}}>
-                <OrbitControls/>
+            <Canvas colorManagement camera={{position:[0,0,170], fov:70}}>
+                <OrbitControls enableZoom={false} />
+                <Lights/>
+                <Plane scale={[100,10,10]} rotation={[0,0,0]}/>
                 <Suspense fallback={null}>
                     <HTMLContent/>
+                    <Html position={[0,0,0]}>
+                        <div style={{background:"rgb(0,0,0,0.5)"}}>
+                            <h2 style={{color:"#fff",right:"50vw"}}>Test</h2>
+                        </div>
+                        
+                    </Html>
                 </Suspense>
             </Canvas>
         </div>
