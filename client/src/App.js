8dd07f94279b579be3d448abcfd9ad6f3477625b
조우성 <ender1412@naver.com>
2021-11-22 01:17:40 +0900

    카테고리클릭시 중첩라우팅 구현

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import {Route,Routes, BrowserRouter} from "react-router-dom"
 import React from "react"
 import NavBar from "../src/components/views/NavBar/NavBar"
 import LandingPage from "../src/components/views/LandingPage/LandingPage"
-import BoardPage from "../src/components/views/BoardPage/BoardPage"
+import BoardPage from "../src/components/views/CategoryPage/CategoryPage"
 import "./css/App.css"
+import CategoryPage from "../src/components/views/CategoryPage/CategoryPage"
 
 export default function App() {
   return (
     <BrowserRouter>
       <NavBar/> 
         <Routes>
+          <Route path=":CatID" element={<BoardPage/>}/>
           <Route path="/" element={<LandingPage/>}/>
-          <Route path="/Boards" element={<BoardPage/>}/>
+          <Route path="/Category/*" element={<CategoryPage/>}/>
         </Routes>
     </BrowserRouter>
 
---
+++ client/src/components/views/CreatePage/CreatePage.js
@@ -0,0 +1,16 @@
+import React,{useState, useEffect} from 'react'
+
+function CreatePage() {
+
+    useEffect(()=>{
+        
+    })
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CreatePage
